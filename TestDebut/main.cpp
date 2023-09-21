3a7807496afdfde94d7f1309b8f6fd620e44cdf2
grenaud- <grenaud-@student.42lausanne.ch>
2023-09-21 11:06:31 +0200

    version cpp de pollserver

---
+++ TestDebut/main.cpp
@@ -0,0 +1,8 @@
+#include "pollserver.hpp"
+
+int main() 
+{
+    PollServer server;
+    server.run();
+    return 0;
+}
--- TestDebut/test.sh
+++ TestDebut/test.sh
@@ -4,6 +4,6 @@ size=$1
 for ((i = 1; i < size; i++))
 do
 #	open -a "Terminal"
-	echo "cochonnerie "$i | nc   localhost 9034
+	echo "cochonnerie "$i | nc   localhost 6667
 done
 
