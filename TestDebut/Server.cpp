02b9942cd347983f7327c5343b37ea98034ba801
jsollet <95758123+jsollet@users.noreply.github.com>
2023-09-21 16:29:37 +0200

    test2

--- TestDebut/Server.cpp
+++ TestDebut/Server.cpp
@@ -28,3 +28,68 @@ int     Server::getPort()
 {
     return this->port;
 }
+
+void    *Server::get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(reinterpret_cast<struct sockaddr_in*>(sa)->sin_addr);
+    }
+    return &(reinterpret_cast<struct sockaddr_in6*>(sa)->sin6_addr);
+}
+
+void    Server::setHint(int family, int type, int flag)
+{
+    this->hints.ai_family = family;
+    this->hints.ai_socktype = type;
+    this->hints.ai_flags = flag;
+}
+
+int     Server::get_listener_socket(void)
+{
+    int listener;     // Listening socket descriptor
+    int yes=1;        // For setsockopt() SO_REUSEADDR, below
+    int rv;
+
+    struct addrinfo *ai, *p;// hints etant prive
+    setHint(AF_UNSPEC, SOCK_STREAM, AI_PASSIVE);
+
+    std::string s;
+    std::stringstream out;
+    out << getPort();
+    s = out.str();
+    const char * port = s.c_str();
+    if ((rv = getaddrinfo(NULL, port, &this->hints, &ai)) != 0) {
+        std::cerr << "selectserver: " << gai_strerror(rv) << std::endl;
+        exit(1);
+    }
+    for(p = ai; p != NULL; p = p->ai_next)
+    {
+        listener = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if (listener < 0) { continue; }
+        setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+        if (bind(listener, p->ai_addr, p->ai_addrlen) < 0) {
+            close(listener);
+            continue;
+        }
+
+        break;
+    }
+    if (p == NULL) { return -1;}
+    freeaddrinfo(ai); // All done with this
+
+    // Listen
+    if (listen(listener, 10) == -1) {return -1;}
+    return listener;
+}
+
+void    Server::add_to_pfds(int newfd)
+{
+    struct pollfd   data;
+
+    data.fd = newfd;
+    data.events = POLLIN;
+
+    this->pdfs.push_back(data);
+
+
+}
--- TestDebut/Server.hpp
+++ TestDebut/Server.hpp
@@ -9,29 +9,52 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <csignal>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <poll.h>
+#include <vector>
 
 
 class Server
 {
 
-    public:
-        Server();
-        ~Server();
-        void    setPort(int port);
-        int     getPort();
-        void    setStop(bool status);
-        void    setPasswd(std::string passwd);
-        void    setTV(int sec, int musec);
-    private:
-        int port;
-        std::string passwd;
+	public:
+		Server();
+		~Server();
+		void	setPort(int port);
+		int		getPort();
+		void	setStop(bool status);
+		void	setPasswd(std::string passwd);
+		void	setTV(int sec, int musec);
 
-        bool stop;
-        struct timeval tv;
+		void	*get_in_addr(struct sockaddr *sa);
+		int		get_listener_socket(void);
+		void	add_to_pfds(int newfd);// a voir
+		void	del_from_pdfs(); // a voir
+		void	setHint(int family, int type, int flag);
+
+	private:
+		int port;
+		std::string passwd;
+		bool stop;
+
+		// variable main...
+		int				listener_socket;//listener
+		int				accepted_socket;//newfd
+		std::string		buffer[512];//char buf[256]
+		std::string		remoteIP[INET6_ADDRSTRLEN];
+		socklen_t		addrlen;
+
+		struct		timeval tv;
+		struct	addrinfo	hints;
+		std::vector<pollfd>	pdfs;
 /*        struct sockaddr_in server_address;
-        struct sockaddr_in client_address;
-        struct sockaddr casted;
+		struct sockaddr_in client_address;
+		struct sockaddr casted;
 */
 };
 
