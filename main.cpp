5c583f460518be1fe71edea8a7909143ef80f460
jsollet <jmsolletti@me.com>
2023-10-23 13:54:59 +0200

    main modifie

--- main.cpp
+++ main.cpp
@@ -4,15 +4,17 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc == 3)
-    {
-        Msg message;
- //       message.test();
-        Server aServer;
-        aServer.setPasswd(argv[2]);
-        aServer.setPort(atoi(argv[1]));
-        aServer.run();
-    }
-    else
-    return (-1);
+	if (argc == 3 && std::string(argv[1]).std::string::find_first_not_of("0123456789",0) == std::string::npos)
+	{
+		//Msg message;// ?
+		Server aServer;
+		aServer.setPasswd(argv[2]);
+		aServer.setPort(atoi(argv[1]));
+		aServer.run();
+	}
+	else
+	{
+		std::cout << red << "Usage: ./Servers port password\n" << reset;
+		return (-1);
+	}
 }
